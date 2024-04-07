d3ebc068d9d6edc379596a9108ed1046bbf2f1c0
lauratrandafir2001 <lauraflorina65@gmail.com>
2024-04-07 06:41:20 +0300

    changes

--- hacknou/home.js
+++ hacknou/home.js
@@ -1,70 +1,70 @@
 var username_string = "temp";
 
 function toggleSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    var toggle = document.querySelector(".sidebar-toggle");
-    sidebar.classList.toggle("active");
-    
-    // Toggle the visibility of the sidebar-toggle button based on sidebar state
-    if (sidebar.classList.contains("active")) {
-      toggle.classList.add("hidden");
-    } else {
-      toggle.classList.remove("hidden");
-    }
+  var sidebar = document.getElementById("sidebar");
+  var toggle = document.querySelector(".sidebar-toggle");
+  sidebar.classList.toggle("active");
+
+  // Toggle the visibility of the sidebar-toggle button based on sidebar state
+  if (sidebar.classList.contains("active")) {
+    toggle.classList.add("hidden");
+  } else {
+    toggle.classList.remove("hidden");
   }
-  
-  // Function to specifically close the sidebar
-  function closeSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    var toggle = document.querySelector(".sidebar-toggle");
-    if (sidebar.classList.contains("active")) {
-      sidebar.classList.remove("active");
-      toggle.classList.remove("hidden");
-    }
+}
+
+// Function to specifically close the sidebar
+function closeSidebar() {
+  var sidebar = document.getElementById("sidebar");
+  var toggle = document.querySelector(".sidebar-toggle");
+  if (sidebar.classList.contains("active")) {
+    sidebar.classList.remove("active");
+    toggle.classList.remove("hidden");
   }
+}
 
 
 function openModal() {
-    var modal = document.getElementById("myModal");
-
-    console.log("open modal");
-    modal.style.display = "block";
-}
-
-function closeModal() {
-    var modal = document.getElementById("myModal");
-    console.log("a intrat aici");
-    modal.style.display = "none";
-}
-
-
-function openModal2() {
   var modal = document.getElementById("myModal");
 
   console.log("open modal");
   modal.style.display = "block";
 }
 
-function closeModal2() {
+function closeModal() {
   var modal = document.getElementById("myModal");
   console.log("a intrat aici");
   modal.style.display = "none";
 }
 
 
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+function openModal2() {
+  var modal = document.getElementById("mysecondModal");
+
+  console.log("open modal");
+  modal.style.display = "block";
+}
+
+function closeModal2() {
+  var modal = document.getElementById("mysecondModal");
+  console.log("a intrat aici");
+  modal.style.display = "none";
+}
+
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
   // Prevent the default form submission behavior
   event.preventDefault();
-  
+
   // Get the values of username and password inputs
   localStorage.setItem('username', document.getElementById('username').value);
   username_string = document.getElementById('username').value;
   var password = document.getElementById('password').value;
-  
+
   // You can now use these variables to perform further actions, such as sending them to a backend server for authentication
   console.log('Username:', username);
   console.log('Password:', password);
-  
+
   // Clear the form fields after submission (optional)
   document.getElementById('username').value = '';
   document.getElementById('password').value = '';
@@ -91,81 +91,81 @@ function addListElements(latitude_value, longitude_value) {
   // localStorage.removeItem('username');
 
   const jsonData = {
-longitude: longitude_value,
-latitude: latitude_value
-};
+    longitude: longitude_value,
+    latitude: latitude_value
+  };
 
-// Define the URL of your backend API endpoint
-const url = 'http://localhost:8080/challenges/all';
+  // Define the URL of your backend API endpoint
+  const url = 'http://localhost:8080/challenges/all';
 
-// Define the options for the fetch request
-const options = {
-method: 'POST',
-headers: {
-'Content-Type': 'application/json'
-},
-body: JSON.stringify(jsonData) // Convert JSON object to a string
-};
+  // Define the options for the fetch request
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(jsonData) // Convert JSON object to a string
+  };
 
-// Send the POST request using fetch
-fetch(url, options)
-.then(response => {
-if (!response.ok) {
-throw new Error('Network response was not ok');
-}
-return response.json(); // Parse the JSON response
-})
-.then(data => {
-// Handle the JSON response data
-console.log('Response received:', data);
-var list = document.getElementById("challenge-list");
-while (list.firstChild) {
-  list.removeChild(list.firstChild);
-}
-for (var i = 0; i < data.length; i++) {
-  //aici creez copii noi
-  var item = document.createElement("button");
-  // item.onclick = function() {
-  //   openModal2();
-  // };
-  item.id = 'ceva';
-  item.className = 'resource-1-link';
+  // Send the POST request using fetch
+  fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Parse the JSON response
+    })
+    .then(data => {
+      // Handle the JSON response data
+      console.log('Response received:', data);
+      var list = document.getElementById("challenge-list");
+      while (list.firstChild) {
+        list.removeChild(list.firstChild);
+      }
+      for (var i = 0; i < data.length; i++) {
+        //aici creez copii noi
+        var item = document.createElement("button");
+        // item.onclick = function() {
+        //   openModal2();
+        // };
+        item.id = 'ceva';
+        item.className = 'resource-1-link';
 
-  var list_item = document.createElement("li");
+        var list_item = document.createElement("li");
 
-  var article_inside = document.createElement("a");
-  article_inside.href = '#';
-  article_inside.innerHTML = data[i].title;
-  article_inside.className = 'resource-1';
+        var article_inside = document.createElement("a");
+        article_inside.href = '#';
+        article_inside.innerHTML = data[i].title;
+        article_inside.className = 'resource-1';
 
-  var description_inside = document.createElement("p");
-  description_inside.innerHTML = data[i].description;
-  description_inside.className = 'challenge-description';
+        var description_inside = document.createElement("p");
+        description_inside.innerHTML = data[i].description;
+        description_inside.className = 'challenge-description';
 
-  var image_inside = document.createElement("img");
-  image_inside.src = './medddddalie.jpg';
-  image_inside.className = 'reward';
-  image_inside.alt = 'Reward';
+        var image_inside = document.createElement("img");
+        image_inside.src = './medddddalie.jpg';
+        image_inside.className = 'reward';
+        image_inside.alt = 'Reward';
 
-  list_item.appendChild(article_inside);
-  list_item.appendChild(description_inside);
-  list_item.appendChild(image_inside);
+        list_item.appendChild(article_inside);
+        list_item.appendChild(description_inside);
+        list_item.appendChild(image_inside);
 
-  item.appendChild(list_item);
-  list.appendChild(item);            
-}
-})
-.catch(error => {
-// Handle errors
-console.error('There was a problem with the fetch operation:', error);
-});
+        item.appendChild(list_item);
+        list.appendChild(item);
+      }
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('There was a problem with the fetch operation:', error);
+    });
 
 
-// var list = document.getElementById("challenge-list");
-// for (var i = 0; i < 5; i++) {
-//     var item = document.createElement("li");
-//     item.innerHTML = "New list item";
-//     list.appendChild(item);            
-// }
+  // var list = document.getElementById("challenge-list");
+  // for (var i = 0; i < 5; i++) {
+  //     var item = document.createElement("li");
+  //     item.innerHTML = "New list item";
+  //     list.appendChild(item);            
+  // }
 
 }
